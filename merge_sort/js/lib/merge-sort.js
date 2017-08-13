615bb31d8d0025cd49139b14965305f3cbd5a6e0
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-13 17:50:30 -0600

    Add pseudocode for merge sort

---
+++ merge_sort/js/lib/merge-sort.js
@@ -0,0 +1,17 @@
+function mergeSort(data) {
+  // base case: if data.length == 2, order the two elements, then return that ordered array.
+  // else:
+	  // split array in half into two arrays
+	  // call mergeSort on each of those
+    // merge the sorted arrays together
+}
+
+function merge(data1, data2) {
+  // compare data1[0] to data2[0]
+  // put them in order in a final sorted array
+  // shift the first item off the beginning of data1 and data2
+  // if data1 and data2 are empty,
+    // return the final sorted array
+}
+
+module.exports = mergeSort;
