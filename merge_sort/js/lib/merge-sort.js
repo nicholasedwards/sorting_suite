6e3322f5db834883fc3b6907546962e7da6ae8fd
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-13 18:46:14 -0600

    Implement merge sort in JavaScript

--- merge_sort/js/lib/merge-sort.js
+++ merge_sort/js/lib/merge-sort.js
@@ -1,17 +1,45 @@
 function mergeSort(data) {
-  // base case: if data.length == 2, order the two elements, then return that ordered array.
-  // else:
-	  // split array in half into two arrays
-	  // call mergeSort on each of those
-    // merge the sorted arrays together
+  if (data.length == 1) {
+    return data;
+  } else {
+
+	  var first_half = splitData({ data: data, operation: 'get-first-half' });
+	  var second_half = splitData({ data: data, operation: 'get-second-half'});
+
+	  var first_half_sorted = mergeSort(first_half);
+	  var second_half_sorted = mergeSort(second_half);
+
+	  var sorted_data = merge(first_half_sorted, second_half_sorted);
+
+	  return sorted_data;
+  }
+}
+
+function splitData(args) {
+  var data = args.data;
+  var operation = args.operation;
+
+  var split_index = Math.round((data.length / 2));
+
+  if (operation == "get-first-half") {
+    return data.slice(0, split_index);
+  } else if (operation == "get-second-half") {
+    return data.slice(split_index, data.length);
+  }
 }
 
 function merge(data1, data2) {
-  // compare data1[0] to data2[0]
-  // put them in order in a final sorted array
-  // shift the first item off the beginning of data1 and data2
-  // if data1 and data2 are empty,
-    // return the final sorted array
+  var merged_data = [];
+  while (data1.length > 0 || data2.length > 0) {
+	  if ( data1[0] < data2[0] || data2.length == 0 ) {
+	    merged_data.push(data1[0]);
+	    data1.shift();
+	  } else if ( data2[0] < data1[0] || data1.length == 0 ) {
+	    merged_data.push(data2[0]);
+	    data2.shift();
+	  }
+  }
+  return merged_data;
 }
 
 module.exports = mergeSort;
--- merge_sort/js/test/merge-sort-test.js
+++ merge_sort/js/test/merge-sort-test.js
@@ -1,3 +1,5 @@
+var pry = require('pryjs');
+
 const assert = require('chai').assert;
 const mergeSort = require('../lib/merge-sort');
 
@@ -5,7 +7,7 @@ describe("Merge Sort", () => {
   context("When supplied with an array of integers", () => {
     it("sorts the array", () => {
       var testNumbers = [1, 5, 3, 2, 6, 4];
-      assert.equal(mergeSort(testNumbers), [1, 2, 3, 4, 5, 6]);
+      assert.deepEqual(mergeSort(testNumbers), [1, 2, 3, 4, 5, 6]);
     });
   });
 });
