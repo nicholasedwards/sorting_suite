cbdbfee54f405382703a3e390852271e2b55fb3a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-13 20:15:08 -0600

    Implement quick sort in JavaScript

--- quick_sort/lib/quick-sort.js
+++ quick_sort/lib/quick-sort.js
@@ -1,15 +1,22 @@
+var pry = require('pryjs');
+
 function quickSort(data) {
-  // base case: if the data array only has one element, return that element
-  // pick a pivot element from the data array
-    // use a random element as the pivot in order to avoid the worst-case
-    // scenario of slow runtime from sorting an already sorted array
-  // declare arrays for holding data which is lesser or greater than the pivot
-  // for each element in the data set:
-    // if it is greater than the pivot, push it onto the greater array
-    // if it is less than the pivot, push it onto the lesser array
-  // recursively call quickSort on the lesser array and the greater array
-  // concatenate the lesser array, the pivot, and the greater array into a final
-  // array, and return that array
+  if (data.length <= 1) {
+    return data;
+  }
+  var pivot_index = Math.floor(Math.random() * data.length);
+  var lesser_than_pivot = [];
+  var greater_than_pivot = [];
+  for (i = 0; i < data.length; i++) {
+	    if (data[i] < data[pivot_index]) {
+	      lesser_than_pivot.push(data[i]);
+	    } else {
+	      greater_than_pivot.push(data[i]);
+	    }
+	  }
+  var sorted_lesser = quickSort(lesser_than_pivot);
+  var sorted_greater = quickSort(greater_than_pivot);
+  return sorted_lesser.concat(sorted_greater);
 }
 
 module.exports = quickSort;
